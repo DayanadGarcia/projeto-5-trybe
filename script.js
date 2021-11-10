45946407407650d68d8001dc9e2329c60429623a
Dayana <dayanadiasgarcia@gmail.com>
2021-11-10 20:55:55 -0300

    nada alterado...

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const btnLogin = document.getElementById("btn-login");
-const btnEnviar = document.getElementById("submit-btn").disable = true;
+const btnEnviar = document.getElementById("submit-btn");
 const checkBox = document.getElementById("agreement");
 
 function validaLogin() {
   const inputEmail = document.getElementById("email-login");
   const inputSenha = document.getElementById("senha-login");
   if (inputEmail.value === "trybe@teste.com" || inputSenha.value === "123456") {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
     alert("Email ou senha inválidos.");
   }
 }
-
+btnEnviar.disabled = true;
 function btnAtivar() {
   if (checkBox.disable === false) {
-    btnEnviar.disable = false;
+    btnEnviar.disabled = false;
   } else if (checkBox.disable === true) {
     btnEnviar.disable = true;
   }
